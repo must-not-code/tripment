4998d8d323a2b7a6e4662ef9c2dd29243cade73b
must-not-code <test4food@me.com>
2020-11-28 16:05:26 +0300

    Add Procedures seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+
+class String
+  def true_strip
+    gsub(/\A[[:space:]]*|[[:space:]]*\z/, '')
+  end
+end
+
+Nokogiri::HTML(URI.open('https://en.wikipedia.org/wiki/Medical_procedure'))
+.xpath("//h3[span[@class='mw-headline']]/following-sibling::ul[1]//li/a[not(@class='new')]").each do |link|
+  try ||= 1
+  response = JSON.load(URI.open("https://en.wikipedia.org/api/rest_v1/page/summary/#{link['href'].sub('/wiki/', '')}"))
+  Procedure.create(
+    title: response['title'].sub('(medicine)', '')&.true_strip,
+    image: response.dig('thumbnail', 'source')&.true_strip,
+    description: response['extract']&.true_strip,
+  )
+rescue
+  try += 1
+  sleep 5
+  try > 3 ? next : retry
+end
